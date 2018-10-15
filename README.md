b1b09aabffb44e8730cb7d2e8c22ba4aecf1211d
Ryan Connolly <rconnolly.work@outlook.com>
2018-10-15 16:15:20 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# CleanCodeBase
+# AspNetCoreTemplate
+
+If you get an System.InvalidOperationException when first running the application, run the following commands:
+
+To generate a developer certificate run 'dotnet dev-certs https'
+To trust the certificate (Windows and macOS only) run 'dotnet dev-certs https --trust'.
---
+++ src/WebApi/Controllers/TokenController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+using Core.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Produces("application/json")]
+    public class TokenController : Controller
+    {
+        [Route("api/v1/token")]
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody]LoginCredentials request)
+        {       
+            return Content(String.Format("Welcome {0}", request.UserName));
+        }
+    }
+}
